7fd6080519a8c3d30de98618db13b380440133b3
123andrew12 <liandrew2003@gmail.com>
2022-03-26 19:54:35 -0400

    Added an events class and functions class

---
+++ SustainabilityKidsGame/src/Events.java
@@ -0,0 +1,20 @@
+
+public class Events {
+	private String text;
+	
+	private void disableFunction(Functions function)
+	{
+		
+	}
+	
+	private void changeCost(Functions function)
+	{
+		
+		
+	}
+	
+	private void setHappiness(int happinessValue)
+	{
+		
+	}
+}
---
+++ SustainabilityKidsGame/src/Functions.java
@@ -0,0 +1,22 @@
+
+public class Functions {
+	String name;
+	int cost;
+	int happiness;
+	int maxAmount;
+	boolean enabled;
+	
+	public Functions(String name, int cost, int happiness, int maxAmount, boolean enabled)
+	{
+		this.name = name;
+		this.cost = cost;
+		this.happiness = happiness;
+		this.maxAmount = maxAmount;
+		this.enabled = enabled;
+	}
+	
+	private void affectEarth()
+	{
+		//Change earth's values and your credit values;
+	}
+}
